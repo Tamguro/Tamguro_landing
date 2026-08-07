47d4a9f8541de36bdee24323f439359b63f0b9a8
이유비 <vincedev@iyubiui-Macmini.local>
2026-08-07 22:45:49 +0900

    fix: allow footer nav/note text to wrap on mobile to avoid overflow

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -19,10 +19,10 @@ export default function Footer(): JSX.Element {
         </div>
 
         <div className="flex flex-col gap-[8px] md:items-end">
-          <p className="font-['Pretendard'] text-[13px] font-medium whitespace-nowrap text-[#1f2430]">
+          <p className="font-['Pretendard'] text-[13px] font-medium md:whitespace-nowrap text-[#1f2430]">
             브랜드 &nbsp;&nbsp; 차별점 &nbsp;&nbsp; 앱 화면 &nbsp;&nbsp; 문의
           </p>
-          <p className="font-['Pretendard'] text-[12px] whitespace-nowrap text-[#667085]">
+          <p className="font-['Pretendard'] text-[12px] md:whitespace-nowrap text-[#667085]">
             학생과 학부모는 소속 교육기관의 안내를 통해 이용합니다.
           </p>
         </div>
